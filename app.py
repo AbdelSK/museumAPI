2a5e026f8b21152dabde669c5f5b9de168f2b71a
Abdelhafid Boutarbouch <aboutarbouch_solofisa@bankinter.com>
2018-09-11 14:17:13 +0200

    first commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import base64
 from flask import Flask, jsonify, request, json
 import numpy as np
 import tensorflow as tf
+from PIL import Image
 
 app = Flask(__name__)
 app.config.from_object(__name__)
