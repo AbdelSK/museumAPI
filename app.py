99c74eb8d0694f290c0b72f45e930f079af5476b
jose <josemanuel@MacBook-Pro-de-jose.local>
2018-09-11 14:43:04 -0700

    app

--- app.py
+++ app.py
@@ -36,34 +36,35 @@ def classify():
 	urllib.request.urlretrieve(imgurl, imgpth) 
     image_data = tf.gfile.FastGFile(imgpth, 'rb').read()    
 	# Loads label file, strips off carriage return
-	label_lines = [line.rstrip() for line
+    label_lines = [line.rstrip() for line
 					   in tf.gfile.GFile("logs/trained_labels.txt")]
 
 	# Unpersists graph from file
-	with tf.gfile.FastGFile("logs/trained_graph.pb", 'rb') as f:
-		graph_def = tf.GraphDef()
-		graph_def.ParseFromString(f.read())
-		_ = tf.import_graph_def(graph_def, name='')
+    with tf.gfile.FastGFile("logs/trained_graph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
 
-	with tf.Session() as sess:
+    with tf.Session() as sess:
 		# Feed the image_data as input to the graph and get first prediction
-		softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
-		predictions = sess.run(softmax_tensor, \
-				 {'DecodeJpeg/contents:0': image_data})
+        predictions = sess.run(softmax_tensor, \
+                 {'DecodeJpeg/contents:0': image_data})
 
 		# Sort to show labels of first prediction in order of confidence
-		top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-		scoreList=[]
-		for node_id in top_k:
-			human_string = label_lines[node_id]
-			score = predictions[0][node_id]
-			print('%s (score = %.5f)' % (human_string, score))
-			scoreList.append({
-				'label': str(human_string),
-				'score': str(score)
-			})
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        scoreList=[]
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+            scoreList.append({
+                'label': str(human_string),
+                'score': str(score)
+            })
 			
-		return json.dumps(scoreList)
+        return json.dumps(scoreList)
+		
 if __name__ == '__main__':
     app.run(debug=True, host= '0.0.0.0', port=8009)
